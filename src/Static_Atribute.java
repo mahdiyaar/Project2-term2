f074116aed99073d2566d689110bfff1def6c4bd
mahdiyaar <m.hosein.assarnia@gmail.com>
2024-05-01 12:56:40 +0330

    creat User

---
+++ src/Static_Atribute.java
@@ -0,0 +1,26 @@
+public class Static_Atribute {
+    private String username,password,email, phoneNumber;
+
+    public Static_Atribute(String username, String password, String email, String phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
--- src/User.java
+++ src/User.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class User {
     private String name,lastname,field;
     private String username,password,email, phoneNumber;
 
-    static A
+    private static ArrayList<Static_Atribute> static_atributes=new ArrayList<>();
+    private boolean staticatribute[]=new boolean[4];
+
 
     public User(String name, String lastname, String field, String username, String password, String email, String phoneNumber) {
+        for (int i = 0; i < 4; i++) {
+            staticatribute[i]=true;
+        }
         if(name.length()<19&& Pattern.matches("[a-zA-Z]",name))
             this.name = name;
         else
@@ -15,10 +21,25 @@ public class User {
             this.lastname = lastname;
         else
             throw new RuntimeException("this lastname is incorrect");
-        if(name.length()<19&& Pattern.matches("[a-zA-Z]",lastname))
+        if(name.length()<19&& Pattern.matches("[a-zA-Z]",field))
             this.field = field;
         else
             throw new RuntimeException("this field is incorrect");
+        for (int i = 0; i < static_atributes.size(); i++) {
+
+        }
+        for (int i = 0; i < static_atributes.size(); i++) {
+            if(username==static_atributes.get(i).getUsername()) {
+                staticatribute[0] = false;
+                break;
+            }
+            if(password==static_atributes.get(i).getPassword()){
+                staticatribute[1] = false;
+                break;
+            }
+            if()
+        }
+        if()
         this.username = username;
         this.password = password;
         this.email = email;
